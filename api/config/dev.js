46a2ffdbf7d42e037df08ab62aeb1902c4bc756c
imad <imad.salki@hotmail.fr>
2021-12-16 23:59:08 +0100

    fix

--- api/config/dev.js
+++ api/config/dev.js
@@ -1,6 +1,6 @@
 module.exports = {
-  googleClientID: '312187773943-4gdh22v5qnjdrjm6amjhe5g5pnmdekf4.apps.googleusercontent.com',
-  googleClientSecret: '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',
+  googleClientID: '**',
+  googleClientSecret: '**',
   mongoURI: 'mongodb://192.168.1.110:27017',
   cookieKey: '123123123',
 };
--- api/services/cache.js
+++ api/services/cache.js
@@ -1,8 +1,10 @@
 
 const redis = require('redis');
 const util = require('util');
-const redisClient = redis.createClient('redis://192.168.1.110:6379');
 const mongoose = require('mongoose');
+
+// Set redis client
+const redisClient = redis.createClient('redis://192.168.1.110:6379');
 
 // Promisify redis client get and set function
 redisClient.get = util.promisify(redisClient.get);
@@ -11,10 +13,22 @@ redisClient.set = util.promisify(redisClient.set);
 // Copy the exec prototype
 const exec = mongoose.Query.prototype.exec;
 
+// Use the cache
+mongoose.Query.prototype.cache = function () {
+
+    this.useCache = true;
+
+    // Return this to allow chain (ex: .cache().limit(10).blabla)
+    return this;
+}
+
 // Override the exec function
 mongoose.Query.prototype.exec = async function () {
 
-    console.log('------------------')
+    // if cache is disabled we return the value from the BDD
+    if (!this.useCache) {
+        return exec.apply(this);
+    }
 
     // Concat the Query and the collection name
     const keys = JSON.stringify(Object.assign({}, this.getQuery(), {
@@ -24,19 +38,15 @@ mongoose.Query.prototype.exec = async function () {
     // Second method to assign 
     // const keys2 = { ...this.getQuery(), ...{ collection: this.mongooseCollection.name } }
 
-    console.log(keys);
-
     // Check if Key exists in cache
     const resultCached = await redisClient.get(keys);
     if (resultCached) {
-        console.log('-- Value found in Redis')
         const doc = JSON.parse(resultCached);
         return Array.isArray(doc) ? doc.map(v => this.model(v)) : new this.model(doc);
     }
 
     // Search for the value
-    console.log('- Search for the result in BDD');
-    const result = await exec.apply(this, arguments);
+    const result = await exec.apply(this);
 
     // Save the result in redis
     await redisClient.set(keys, JSON.stringify(result))
